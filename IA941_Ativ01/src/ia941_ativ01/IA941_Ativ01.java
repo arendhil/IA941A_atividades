2e7b6b380f80d4568a12cd8ed9333fefa8ec1a87
Aluno IA941 <ia941@le25-20.grad.fee.unicamp.br>
2019-03-11 15:22:01 -0300

    Starting class

---
+++ IA941_Ativ01/src/ia941_ativ01/IA941_Ativ01.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ia941_ativ01;
+
+/**
+ *
+ * @author ia941
+ */
+public class IA941_Ativ01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
